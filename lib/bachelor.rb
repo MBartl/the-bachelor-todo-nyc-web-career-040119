aad839ae8f6c94d7ca5ebb3bc22248ec86ff8d22
Matthew Bartl <mattbartl@gmail.com>
2019-03-22 18:50:41 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -25,7 +25,7 @@ def get_contestant_name(data, occupation)
   end
 end
 
-get_contestant_name(data, "Psychiatric Nurse")
+get_contestant_name(hash, "Psychiatric Nurse")
 
 
 def count_contestants_by_hometown(data, hometown)
