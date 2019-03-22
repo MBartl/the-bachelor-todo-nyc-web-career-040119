ce2edb6a25119eca32ccdd76f6437458920bac43
Matthew Bartl <mattbartl@gmail.com>
2019-03-22 19:27:59 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -14,4 +14,59 @@ def get_first_name_of_season_winner(data, season_input)
 end
 
 
+def get_contestant_name(data, occupation)
+  data.each do |season, contestant_data|
+    i = 0
+    while i < contestant_data.length do
+      if contestant_data[i][:occupation].to_s == occupation
+        return contestant_data[i][:name].to_s
+      end
+      i += 1
+    end
+  end
+end
+
+
+def count_contestants_by_hometown(data, hometown)
+  hometown_counter = 0
+  data.each do |season, contestant_data|
+    i = 0
+    while i < contestant_data.length do
+      if contestant_data[i][:hometown].to_s == hometown
+        hometown_counter += 1
+      end
+      i += 1
+    end
+  end
+  return hometown_counter
+end
+
+
+def get_occupation(data, hometown)
+  data.each do |season, contestant_data|
+    i = 0
+    while i < contestant_data.length do
+      if contestant_data[i][:hometown].to_s == hometown
+        return contestant_data[i][:occupation].to_s
+      end
+      i += 1
+    end
+  end
+end
+
+
+def get_average_age_for_season(data, season_input)
+  rounded_age = 0.0
+  i = 0
+  data.each do |season, contestant_data|
+    if season.to_s == season_input
+      while i < contestant_data.length do
+        rounded_age += contestant_data[i][:age].to_i
+        i += 1
+      end
+    end
+  end
+  rounded_age = rounded_age/i
+  return rounded_age.round
+end
 
