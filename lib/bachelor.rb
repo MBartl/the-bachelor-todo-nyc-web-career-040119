a9516c07ffc9136a7e7d3098766aed5fc07922e2
Matthew Bartl <mattbartl@gmail.com>
2019-03-22 16:21:07 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,7 @@
 def get_first_name_of_season_winner(data, season_input)
   data.each do |season, contestant|
     if season_input == season
+      return season
       contestant.each do 
       return contestant[:name]
     end
